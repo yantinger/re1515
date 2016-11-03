76d7614239c544b52f4c563333223e084da316fb
StanleyHuangEE <stanley850904@gmail.com>
2016-11-03 22:15:52 +0800

    add sidebar

---
+++ jQery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
+
+});
